2165e63c3570b45d0eb6cc231feef9183632cb9b
zhch <zcy777et@gmail.com>
2025-12-20 12:38:58 +0800

    fix(): remove llm api

--- src/app/[locale]/(home)/blog/[[...slug]]/page.tsx
+++ src/app/[locale]/(home)/blog/[[...slug]]/page.tsx
@@ -1,10 +1,9 @@
 import { getMDXComponents } from '@/components/mdx-components';
 import { appConfig } from '@/lib/appConfig';
-import { NotFoundPage } from '@windrun-huaiin/base-ui/components';
-import { blogSource } from '@/lib/source-blog';
-import { createFumaPage } from '@windrun-huaiin/third-ui/fuma/server';
 import { SiteIcon } from '@/lib/site-config';
-import { LLMCopyButton } from '@windrun-huaiin/third-ui/fuma/mdx';
+import { blogSource } from '@/lib/source-blog';
+import { NotFoundPage } from '@windrun-huaiin/base-ui/components';
+import { createFumaPage } from '@windrun-huaiin/third-ui/fuma/server';
 
 const sourceKey = 'blog';
 const { Page, generateStaticParams, generateMetadata } = createFumaPage({
@@ -12,12 +11,10 @@ const { Page, generateStaticParams, generateMetadata } = createFumaPage({
   mdxContentSource: blogSource,
   getMDXComponents,
   mdxSourceDir: appConfig.mdxSourceDir[sourceKey],
-  githubBaseUrl: appConfig.githubBaseUrl,
-  copyButtonComponent: <LLMCopyButton />,
   siteIcon: <SiteIcon />,
   FallbackPage: NotFoundPage,
   supportedLocales: appConfig.i18n.locales as string[],
 });
 
 export default Page;
-export { generateStaticParams, generateMetadata };
+export { generateMetadata, generateStaticParams };
